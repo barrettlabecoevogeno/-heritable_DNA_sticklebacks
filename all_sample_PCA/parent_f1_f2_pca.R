c52d10654c5918b5eb5457284e15a6bd893b9642
cauls19900319 <juntao.hu@mail.mcgill.ca>
2020-10-26 15:56:05 +0800

    Update parent_f1_f2_pca.R

--- all_sample_PCA/parent_f1_f2_pca.R
+++ all_sample_PCA/parent_f1_f2_pca.R
@@ -279,13 +279,15 @@ pca_plot
 
 
 
-## Supplemental material figure S1
+## Supplemental material figure S1------
 library(ggbiplot)
 # By generation and line
 Line=info$Origin
 Line=factor(Line, levels = c("BI", "HL", "KL"), ordered = T)
 Generation=info$Generation
 Generation=factor(Generation, levels = c("P", "F1", "F2"), ordered = T)
+Label=info$Label
+Label[c(1:83,88:94)]=NA
 
 s1a=ggbiplot(pca,
               obs.scale = 1, 
@@ -296,7 +298,8 @@ s1a=ggbiplot(pca,
 s1a=s1a + geom_point(aes(color=Generation, fill=Generation, shape=Line), size=3)+
   scale_color_manual(values = c("#999999", "#009E73", "#D55E00"))+
   scale_fill_manual(values = c("#999999", "#009E73", "#D55E00"))+
-  scale_shape_manual(values = c(21:22, 24))
+  scale_shape_manual(values = c(21:22, 24))+
+  geom_text(aes(label=Label), hjust=-0.15, size=3)
 s1a=s1a + theme_bw()+theme(panel.grid=element_blank(),
                          panel.grid.major = element_blank(),
                          panel.grid.minor = element_blank(),
@@ -314,7 +317,8 @@ s2a=ggbiplot(pca,
 s2a=s2a + geom_point(aes(color=Generation, fill=Generation, shape=Lane), size=3)+
   scale_color_manual(values = c("#999999", "#009E73", "#D55E00"))+
   scale_fill_manual(values = c("#999999", "#009E73", "#D55E00"))+
-  scale_shape_manual(values = c(21:24))
+  scale_shape_manual(values = c(21:24))+
+  geom_text(aes(label=Label), hjust=-0.15, size=3)
 s2a=s2a + theme_bw()+theme(panel.grid=element_blank(),
                            panel.grid.major = element_blank(),
                            panel.grid.minor = element_blank(),
@@ -322,8 +326,189 @@ s2a=s2a + theme_bw()+theme(panel.grid=element_blank(),
 s2a
 
 library(cowplot)
-Fig.S1=plot_grid(s1a, s2a)
+Fig.S1=plot_grid(s1a, s2a,
+                 labels = c("a", "b"))
 Fig.S1
+
+# track the methylation levels of the 891 DMCs identified in parental generation across F1 and F2 generation----------
+CpG.all.meth=as.data.frame(perc.meth.all)
+p_dmcs_loc=read.table("dmcs_parental.txt")
+colnames(p_dmcs_loc)=c("chr", "start", "end")
+p_dmcs_loc_in_all_meth=findOverlaps(as(p_dmcs_loc, "GRanges"), as(unite_norm_10x_CT_GA_nosex_DB, "GRanges"))
+p_dmcs_loc_in_all_meth=data.frame(p_dmcs_loc_in_all_meth)
+
+CpG.p.dmcs.in.all.meth=CpG.all.meth[p_dmcs_loc_in_all_meth$subjectHits,]
+axes.all$Label==colnames(CpG.p.dmcs.in.all.meth)
+
+library(pheatmap)
+sampleinfo=data.frame(Line=axes.all$Origin,
+                      Generation=axes.all$Generation)
+rownames(sampleinfo)=colnames(CpG.p.dmcs.in.all.meth)
+# levels(sampleinfo$Habitat)[2]="Freshwater"
+ann_colors=list(Generation=c(P="#00BFC4", F1="#F8766D", F2="#009E73"),
+                Line=c(BI="#000000", HL="#CCCCCC", KL="#999999"))
+p.heatmap=pheatmap(CpG.p.dmcs.in.all.meth, 
+                   cluster_rows = TRUE, 
+                   show_rownames = FALSE,
+                   show_colnames = TRUE,
+                   cluster_cols = TRUE,
+                   border_color = NA,
+                   scale = "row",
+                   clustering_distance_rows = "euclidean",
+                   clustering_distance_cols = "euclidean",
+                   clustering_method = "complete",
+                   annotation_col = sampleinfo,
+                   annotation_colors = ann_colors,
+                   annotation_names_col = F)
+
+# comparing the changes of methylation levels of the 891 DMCs across generations
+index_p_BI=as.numeric(rownames(axes.all[axes.all$Generation=="P" & axes.all$Origin=="BI",]))
+index_p_KL=as.numeric(rownames(axes.all[axes.all$Generation=="P" & axes.all$Origin=="KL",]))
+index_p_HL=as.numeric(rownames(axes.all[axes.all$Generation=="P" & axes.all$Origin=="HL",]))
+index_f1_KL=as.numeric(rownames(axes.all[axes.all$Generation=="F1" & axes.all$Origin=="KL",]))
+index_f1_HL=as.numeric(rownames(axes.all[axes.all$Generation=="F1" & axes.all$Origin=="HL",]))
+index_f2_KL=as.numeric(rownames(axes.all[axes.all$Generation=="F2" & axes.all$Origin=="KL",]))
+index_f2_HL=as.numeric(rownames(axes.all[axes.all$Generation=="F2" & axes.all$Origin=="HL",]))
+
+meth_p_BI=CpG.p.dmcs.in.all.meth[,index_p_BI]
+meth_p_KL=CpG.p.dmcs.in.all.meth[,index_p_KL]
+meth_p_HL=CpG.p.dmcs.in.all.meth[,index_p_HL]
+meth_f1_KL=CpG.p.dmcs.in.all.meth[,index_f1_KL]
+meth_f1_HL=CpG.p.dmcs.in.all.meth[,index_f1_HL]
+meth_f2_KL=CpG.p.dmcs.in.all.meth[,index_f2_KL]
+meth_f2_HL=CpG.p.dmcs.in.all.meth[,index_f2_HL]
+
+meth_p_BI$mean=apply(meth_p_BI, 1, mean)
+meth_p_KL$mean=apply(meth_p_KL, 1, mean)
+meth_p_HL$mean=apply(meth_p_HL, 1, mean)
+meth_f1_KL$mean=apply(meth_f1_KL, 1, mean)
+meth_f1_HL$mean=apply(meth_f1_HL, 1, mean)
+meth_f2_KL$mean=apply(meth_f2_KL, 1, mean)
+meth_f2_HL$mean=apply(meth_f2_HL, 1, mean)
+
+meth_f1_KL$between=ifelse(meth_f1_KL$mean>meth_p_BI$mean & meth_f1_KL$mean<meth_p_KL$mean, "yes",
+                          ifelse(meth_f1_KL$mean<meth_p_BI$mean & meth_f1_KL$mean>meth_p_KL$mean, "yes", "no"))
+
+nrow(meth_f1_KL[meth_f1_KL$between=="yes",]) # 694
+nrow(meth_f1_KL[meth_f1_KL$between=="no",]) # 197
+
+library(DescTools)
+expected=c(0.5,1-0.5)
+observed=c(694,197)
+
+GTest(x=observed,
+      p=expected,
+      correct="none") # G = 293.76, X-squared df = 1, p-value < 2.2e-16
+
+# sex bias
+meth_f1_KL$diff=abs(meth_f1_KL$mean-meth_p_BI$mean)-abs(meth_f1_KL$mean-meth_p_KL$mean)
+meth_f1_KL$bias=ifelse(meth_f1_KL$diff>0 & meth_f1_KL$between=="yes", "father", 
+                       ifelse(meth_f1_KL$diff<0 & meth_f1_KL$between=="yes", "mother", "no_bias"))
+
+nrow(meth_f1_KL[meth_f1_KL$bias=="father",]) # 357
+nrow(meth_f1_KL[meth_f1_KL$bias=="mother",]) # 337
+nrow(meth_f1_KL[meth_f1_KL$bias=="no_bias",]) # 197
+
+library(DescTools)
+expected=c(0.5,1-0.5)
+observed=c(357,337)
+
+GTest(x=observed,
+      p=expected,
+      correct="none") # G = 0.57645, X-squared df = 1, p-value = 0.4477
+# no bias in sex in KL F1
+
+meth_f1_HL$between=ifelse(meth_f1_HL$mean>meth_p_BI$mean & meth_f1_HL$mean<meth_p_HL$mean, "yes",
+                          ifelse(meth_f1_HL$mean<meth_p_BI$mean & meth_f1_HL$mean>meth_p_HL$mean, "yes", "no"))
+
+nrow(meth_f1_HL[meth_f1_HL$between=="yes",]) # 610
+nrow(meth_f1_HL[meth_f1_HL$between=="no",]) # 281
+
+library(DescTools)
+expected=c(0.5,1-0.5)
+observed=c(610,281)
+
+GTest(x=observed,
+      p=expected,
+      correct="none") # G = 124.41, X-squared df = 1, p-value < 2.2e-16
+
+meth_f1_HL$diff=abs(meth_f1_HL$mean-meth_p_BI$mean)-abs(meth_f1_HL$mean-meth_p_HL$mean)
+meth_f1_HL$bias=ifelse(meth_f1_HL$diff>0 & meth_f1_HL$between=="yes", "father", 
+                       ifelse(meth_f1_HL$diff<0 & meth_f1_HL$between=="yes", "mother", "no_bias"))
+
+nrow(meth_f1_HL[meth_f1_HL$bias=="father",]) # 280
+nrow(meth_f1_HL[meth_f1_HL$bias=="mother",]) # 330
+nrow(meth_f1_HL[meth_f1_HL$bias=="no_bias",]) # 281
+
+library(DescTools)
+expected=c(0.5,1-0.5)
+observed=c(280,330)
+
+GTest(x=observed,
+      p=expected,
+      correct="none") # G = 4.103, X-squared df = 1, p-value = 0.04281 
+# more sites bias toward mother in HL F1
+
+### F2 vs. parental
+meth_f2_KL$between=ifelse(meth_f2_KL$mean>meth_p_BI$mean & meth_f2_KL$mean<meth_p_KL$mean, "yes",
+                          ifelse(meth_f2_KL$mean<meth_p_BI$mean & meth_f2_KL$mean>meth_p_KL$mean, "yes", "no"))
+
+nrow(meth_f2_KL[meth_f2_KL$between=="yes",]) # 487
+nrow(meth_f2_KL[meth_f2_KL$between=="no",]) # 404
+
+meth_f2_HL$between=ifelse(meth_f2_HL$mean>meth_p_BI$mean & meth_f2_HL$mean<meth_p_HL$mean, "yes",
+                          ifelse(meth_f2_HL$mean<meth_p_BI$mean & meth_f2_HL$mean>meth_p_HL$mean, "yes", "no"))
+
+nrow(meth_f2_HL[meth_f2_HL$between=="yes",]) # 578
+nrow(meth_f2_HL[meth_f2_HL$between=="no",]) # 313
+
+### F2 vs. F1
+meth_f2_KL$betweenf1=ifelse(meth_f2_KL$mean>meth_f1_KL$KL1F & meth_f2_KL$mean<meth_f1_KL$KL1M, "yes",
+                          ifelse(meth_f2_KL$mean>meth_f1_KL$KL1F & meth_f2_KL$mean<meth_f1_KL$KL1M, "yes", "no"))
+
+nrow(meth_f2_KL[meth_f2_KL$betweenf1=="yes",]) # 219
+nrow(meth_f2_KL[meth_f2_KL$betweenf1=="no",]) # 672
+
+meth_f2_HL$betweenf1=ifelse(meth_f2_HL$mean>meth_f1_HL$HL2M3M & meth_f2_HL$mean<meth_f1_HL$HL2F, "yes",
+                          ifelse(meth_f2_HL$mean<meth_f1_HL$HL2M3M & meth_f2_HL$mean>meth_f1_HL$HL2F, "yes", "no"))
+
+nrow(meth_f2_HL[meth_f2_HL$betweenf1=="yes",]) # 399
+nrow(meth_f2_HL[meth_f2_HL$betweenf1=="no",]) # 492
+
+# sex bias analysis
+meth_f2_KL$diff=abs(meth_f2_KL$mean-meth_f1_KL$KL1F)-abs(meth_f2_KL$mean-meth_f1_KL$KL1M)
+meth_f2_KL$bias=ifelse(meth_f2_KL$diff>0 & meth_f2_KL$betweenf1=="yes", "father", 
+                       ifelse(meth_f2_KL$diff<0 & meth_f2_KL$betweenf1=="yes", "mother", "no_bias"))
+
+nrow(meth_f2_KL[meth_f2_KL$bias=="father",]) # 95
+nrow(meth_f2_KL[meth_f2_KL$bias=="mother",]) # 124
+nrow(meth_f2_KL[meth_f2_KL$bias=="no_bias",]) # 672
+
+library(DescTools)
+expected=c(0.5,1-0.5)
+observed=c(95,124)
+
+GTest(x=observed,
+      p=expected,
+      correct="none") # G = 3.8515, X-squared df = 1, p-value = 0.0497
+# more sites bias toward mother in KL F2
+
+meth_f2_HL$diff=abs(meth_f2_HL$mean-meth_f1_HL$HL2F)-abs(meth_f2_HL$mean-meth_f1_HL$HL2M3M)
+meth_f2_HL$bias=ifelse(meth_f2_HL$diff>0 & meth_f2_HL$betweenf1=="yes", "father", 
+                       ifelse(meth_f2_HL$diff<0 & meth_f2_HL$betweenf1=="yes", "mother", "no_bias"))
+
+nrow(meth_f2_HL[meth_f2_HL$bias=="father",]) # 202
+nrow(meth_f2_HL[meth_f2_HL$bias=="mother",]) # 197
+nrow(meth_f2_HL[meth_f2_HL$bias=="no_bias",]) # 492
+
+library(DescTools)
+expected=c(0.5,1-0.5)
+observed=c(202,197)
+
+GTest(x=observed,
+      p=expected,
+      correct="none") # G = 0.062658, X-squared df = 1, p-value = 0.8023
+# no bias in sex in HL F2
 
 
 
